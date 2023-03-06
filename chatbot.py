368c0469f4f163733dfb6684c61a3cce64f0fa13
marcello <marcello.goccia@outlook.com>
2023-03-06 11:39:23 +0800

    testing commend for pair programming

--- chatbot.py
+++ chatbot.py
@@ -2,11 +2,13 @@ import spacy
 
 
 def tokenize_spacy(sentence):
+    pass
     # Load the English language model
     nlp = spacy.load("en_core_web_sm")
 
     # Process the input sentence with spaCy
     doc = nlp(sentence)
+    # this comment just added now!!
 
     # Extract the tokens from the processed document
     tokens = [f"{token.text} {token.pos_}" for token in doc]
@@ -15,8 +17,8 @@ def tokenize_spacy(sentence):
     print(tokens)
 
     return tokens
-
-
-# Example input sentence
-sentence = "The quick brown fox jumps over the lazy dog."
+#
+#
+# # Example input sentence
+sentence = "The quick brown foxes jumped over the lazy dogs."
 tokenize_spacy(sentence)
